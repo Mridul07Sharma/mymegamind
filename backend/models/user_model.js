61ad062fc5a001d3311c4ff54c585ab7e0907fe3
prem prakash gupta <guptapraksh18@gmail.com>
2022-05-27 13:38:45 +0530

    login,signup and reset password feature

---
+++ backend/models/user_model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+
+      trim: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
+    memberType: {
+      type: String,
+    },
+    token: {
+      type: String,
+      default: "token",
+    },
+  },
+  { timeStamp: true }
+);
+
+module.exports = mongoose.model("UserSchema", UserSchema);
---
+++ frontend/component/Dashboard/Faculty_dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Faculty_dashboard = () => {
+  return <div>Faculty_dashboard</div>;
+};
+
+export default Faculty_dashboard;
